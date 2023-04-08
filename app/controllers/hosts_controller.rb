cfe4b1faa945f8f579998875d9b01308520ed579
EC2 Default User <ec2-user@ip-172-31-47-113.ap-northeast-1.compute.internal>
2023-04-08 09:07:55 +0000

    Finish

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,4 +1,25 @@
 class HostsController < ApplicationController
   def top
   end
+
+  def new
+    @host = Host.new
+  end
+
+  def create
+    host = Host.new(host_params)
+    host.save
+    redirect_to '/top'
+  end
+
+  def index
+    @hosts = Host.all
+  end
+
+
+  private
+
+  def host_params
+    params.require(:host).permit(:title, :body)
+  end
 end
